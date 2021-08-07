689d3678ac8ae76c30058db2497d1ed9cb59b122
IndraSubagja <69548809+IndraSubagja@users.noreply.github.com>
2021-08-07 18:20:09 +0700

    Update Player.mjs

--- secure-real-time-multiplayer-game/public/Player.mjs
+++ secure-real-time-multiplayer-game/public/Player.mjs
@@ -1,18 +1,39 @@
 class Player {
   constructor({x, y, score, id}) {
-
+    this.x = x
+    this.y = y
+    this.score = score
+    this.id = id
   }
 
   movePlayer(dir, speed) {
-
+    switch(dir) {
+      case 'up':
+        this.y -= speed
+        return { x: this.x, y: this.y }
+      case 'down':
+        this.y += speed
+        return { x: this.x, y: this.y }
+      case 'left':
+        this.x -= speed
+        return { x: this.x, y: this.y }
+      case 'right':
+        this.x += speed
+        return { x: this.x, y: this.y }
+      default:
+        return { x: this.x, y: this.y }
+    }
   }
 
   collision(item) {
-
+    return this.x === item.x && this.y === item.y ? true : false
   }
 
   calculateRank(arr) {
+    arr.sort((a, b) => b.score - a.score)
+    const rank = arr.findIndex(player => player.score === this.score) + 1
 
+    return `Rank: ${rank} / ${arr.length}`
   }
 }
 
